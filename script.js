c4ecbf7bd73dcfa123677a3718ebc1d67bf712f6
James Gibb <51032141+jamesrgibb@users.noreply.github.com>
2019-09-10 10:02:12 -0600

    updates

--- script.js
+++ script.js
@@ -1,70 +1,59 @@
-'use strict';
 
-const apiKey = 'lxOfNNkQf1LO90abfbiWHOBU4sCkbYFszVyB08uQ';
-const url = 'https://developer.nps.gov/api/v1/parks';
+function getParks(searchName, maxResults){ 
+  const apiKey = 'lxOfNNkQf1LO90abfbiWHOBU4sCkbYFszVyB08uQ';
+  const url = 'https://developer.nps.gov/api/v1/parks';
 
-fetch(
-  url + '?stateCode=ca&limit=10', {
-    headers: {
-      'X-Api-Key': apiKey
-    }
+  const params = formatQueryParams({
+    api_key: apiKey,
+    limit: maxResults,
+    q: searchName
   })
-    .then(res => res.json())
-    .then(res => console.log(res))
 
-// function formatQueryParams(params){
-//     const queryItems = Object.keys(params)
-//     .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
-//     return queryItems.join('&');
-// }
-// function displayResults(responseJson){
-//     console.log(responseJson);
-// 
-//     $('#results-list').empty();
-// 
-//     for(let i = 0; i< responseJson.items.length; i++){
-//         $('#results-list').append(
-//             `<li><h3>${responseJson.data[i].states.name}</h3>
-//       <p>${responseJson.data[i].states.description}</p>
-//       <a href='${responseJson.data[i].states.default.url}'>
-//       </li>`
-//     )};
-//     $('#results').removeClass('hidden');
-// }
-// 
-// function getParks (query, maxResults=10) {
-//     const params = {
-//         api_key: apiKey,
-//         // stateCode: query,
-//         limit: maxResults,
-//         q: query
-//     };
-//     console.log(params)
-//     const queryString = formatQueryParams(params)
-//     const newUrl = url + '?' + queryString;
-//     fetch(newUrl)
-//     .then(response =>{
-//         if(response.ok){   
-//             return response.json();
-//         }
-//         throw new Error(response.statusText);
-//     })
-//     .then(console.log)
-//     // .then(responseJson =>displayResults(responseJson))
-//     .catch(err => {
-//         $('#js-error-message').text(`Something went wrong: ${err.message}`);
-//     });
-// }
-// function watchForm() {
-// $('form').submit(event=> {
-//     event.preventDefault()
-//     const searchName = $('#js-search-state').val();
-//     const maxResults = $('#js-max-results').val();
-//     getParks(searchName, maxResults);
-// });
-// }
-// $(function(){
-//     console.log('app loaded');
-//     $(watchForm);
-// })
-// 
+  return fetch(`${url}?${params}`)
+    .then(response =>{
+        if(!response.ok)   
+          throw new Error(response.statusText);
+        return response.json();
+    })
+    .then(renderParks)
+    .catch(renderError)
+}
+
+function renderError(err) {
+  return $('#js-error-message')
+    .text(`Something went wrong: ${err.message}`);
+}
+
+function formatQueryParams(params){
+    const queryItems = 
+      Object.keys(params)
+        .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+    return queryItems.join('&');
+}
+
+function renderParks(responseJson){
+  console.log(responseJson)
+  $('#results-list').empty();
+  for(let i = 0; i< responseJson.items.length; i++){
+      $('#results-list').append(
+          `<li><h3>${responseJson.data[i].states.name}</h3>
+    <p>${responseJson.data[i].states.description}</p>
+    <a href='${responseJson.data[i].states.default.url}'>
+    </li>`
+  )};
+  return $('#results').removeClass('hidden');
+}
+
+function watchForm() {
+  $('form').submit(event=> {
+    event.preventDefault()
+    const searchName = $('#js-search-state').val();
+    const maxResults = $('#js-max-results').val();
+    getParks(searchName, maxResults);
+  });
+}
+
+$(function(){
+    console.log('app loaded');
+    $(watchForm);
+})
